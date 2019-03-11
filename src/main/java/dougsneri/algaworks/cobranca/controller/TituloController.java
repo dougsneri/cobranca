ed2d1e1f9f83fc0c6835f7946e62eab28366e3b7
dougsneri <dsds.douglas@gmail.com>
2019-03-11 16:06:36 -0300

    Ajustes ate a aula 2.5 (nao a 2.5)

--- src/main/java/dougsneri/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/dougsneri/algaworks/cobranca/controller/TituloController.java
@@ -1,14 +1,19 @@
 package dougsneri.algaworks.cobranca.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import dougsneri.algaworks.cobranca.model.Titulo;
+import dougsneri.algaworks.cobranca.repository.Titulos;
 
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
+
+	@Autowired
+	private Titulos titulos;
 	
 	@RequestMapping("/novo")
 	public String novo() {
@@ -16,7 +21,9 @@ public class TituloController {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public void salvar(Titulo titulo) {
-		// TODO: Salvar no banco de dados
+	public String salvar(Titulo titulo) {
+
+		titulos.save(titulo);
+		return "CadastroTitulo";
 	}
 }
