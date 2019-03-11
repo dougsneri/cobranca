d9e00a3689099d2f8b514a5347ba05e058cfef21
dougsneri <dsds.douglas@gmail.com>
2019-03-11 11:44:06 -0300

    Melhorando a tela e fazendo primeiro metodo POST

--- src/main/java/dougsneri/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/dougsneri/algaworks/cobranca/controller/TituloController.java
@@ -2,13 +2,21 @@ package dougsneri.algaworks.cobranca.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import dougsneri.algaworks.cobranca.model.Titulo;
 
 @Controller
+@RequestMapping("/titulos")
 public class TituloController {
 	
-	@RequestMapping("/titulos/novo")
+	@RequestMapping("/novo")
 	public String novo() {
 		return "CadastroTitulo";
 	}
-	
+
+	@RequestMapping(method = RequestMethod.POST)
+	public void salvar(Titulo titulo) {
+		// TODO: Salvar no banco de dados
+	}
 }
